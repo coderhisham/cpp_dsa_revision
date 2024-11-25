b93a6074fe56f4e56249f5469ed406df772cfbf3
coderhisham <imhish4m@gmail.com>
2024-11-25 09:00:29 +0530

    first commit

--- DSA/Arrays/01Insertion.cpp
+++ DSA/Arrays/01Insertion.cpp
@@ -5,9 +5,13 @@ int main(){
   int n;
   cin>>n;
   int a[n];
+
+ //action: Insertion
   for(int i=0;i<n;i++){
     cin>>a[i];
   }
+
+  //action: Traversal and Printing
   for(int i=0;i<n;i++){
     cout<<a[i]<<" ";
   }
